d2e8eb0e0375658a71d225a739e84a8b43976c91
MoisesGomez0 <gmoises926@gmail.com>
2019-09-19 00:20:28 -0600

    Ejecutar principal.py, esta version da la opcion de un nuevo archivo crearlo y pude modificar y guardar nuevos archivos, tambien crea el grafo

--- PrototiposProyectoGrafos/Graph.py
+++ PrototiposProyectoGrafos/Graph.py
@@ -22,13 +22,23 @@ class Graph:
         if vertex1 not in self.json[vertex2]:
             self.json[vertex2].append(vertex1)
 
+    def buildGraph(self,content):
+        content = content.split("\n")
+        for i in range(len(content) - 1):
+            if content[i].count("\t") == 0:
+                self.addVertex(content[i].lstrip("\t"))
+            if content[i + 1].count("\t") == content[i].count("\t") + 1:
+                for j in range(i + 1, len(content) - 1):
+                    self.addEdge(content[i].lstrip("\t"), content[j].lstrip("\t"))
+                    if content[j + 1].count("\t") == content[i].count("\t"):
+                        break
+
     def showDraw(self):
         for vertice, aristas in self.json.items():
             self.fig.add_node("%s" % vertice)
             for arista in aristas:
                 self.fig.add_node("%s" % arista)
                 self.fig.add_edge("%s" % vertice, "%s" % arista, weight=1)
-
         position = nx.spring_layout(self.fig)
         labels={(vertex1,vertex2): weigth["weight"] for vertex1,vertex2,weigth in self.fig.edges(data=True)}
         nx.draw(self.fig,position, with_labels=True)
